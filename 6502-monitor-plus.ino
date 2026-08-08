3b8a5e26d19486a5baa90563c227971e353eac3f
idunmore <62761638+idunmore@users.noreply.github.com>
2026-08-08 11:03:39 -0400

    Fix missing comma in OPCODES array

--- 6502-monitor-plus.ino
+++ 6502-monitor-plus.ino
@@ -38,7 +38,7 @@ const char OPCODES[256][12] PROGMEM = {
   "CLC",      "ORA abs,Y",   "INC",       "???",       "TRB abs",      "ORA abs,X",  "ASL abs,X",  "BBR1 rel",     // 18
   "JSR abs",  "AND (zp,X)",  "???",       "???",       "BIT zp",       "AND zp",     "ROL zp",     "RMB2 zp",      // 20
   "PLP",      "AND #",       "ROL",       "???",       "BIT abs",      "AND abs",    "ROL abs",    "BBR2 rel",     // 28
-  "BMI rel"   "AND (zp),Y",  "AND (zp)",  "???",       "BIT zp,X",     "AND zp,X",   "ROL zp,X",   "RMB3 zp",      // 30
+  "BMI rel",  "AND (zp),Y",  "AND (zp)",  "???",       "BIT zp,X",     "AND zp,X",   "ROL zp,X",   "RMB3 zp",      // 30
   "SEC",      "AND abs,Y",   "DEC",       "???",       "BIT abs,X",    "AND abs,X",  "ROL abs,X",  "BBR3 el",      // 38
   "RTI",      "EOR (zp,X)",  "???",       "???",       "???",          "EOR zp",     "LSR zp",     "RMB4 zp",      // 40
   "PHA",      "EOR #",       "LSR",       "???",       "JMP abs",      "EOR abs",    "LSR abs",    "BBR4 rel",     // 48
